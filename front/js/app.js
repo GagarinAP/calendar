e415985976a745261d157b4f77937f8ddfce35d0
GagarinAP <gagarin.web@gmail.com>
2017-03-03 10:40:48 +0200

    fix main page and phones

--- front/js/app.js
+++ front/js/app.js
@@ -2,7 +2,38 @@ var app = angular.module("app", [
   "ngRoute",
   "ui.bootstrap",
   "ngResource"
-]);
+])
+.controller("appCtrl", ["$scope","$http","$location",function($scope,$http,$location){
+  $http.get('./phones.json').then(successCallback, errorCallback);
+  function successCallback(response){
+    $scope.phones = response.data;
+  };
+  function errorCallback(error){
+    console.log(error);
+  };  
+}])
+.controller("aboutCtrl", ["$scope","$http","$location",function($scope,$http,$location){
+}])
+.controller("phonesCtrl", ["$scope","$http","$location",function($scope,$http,$location){
+}])
+.controller("servicesCtrl", ["$scope","$http","$location",function($scope,$http,$location){
+}])
+.controller("contactCtrl", ["$scope","$http","$location",function($scope,$http,$location){
+}])
+.controller("phoneCtrl", ["$scope","$http","$location", "$routeParams",function($scope,$http,$location,$routeParams){
+    $scope.phoneId = $routeParams.phoneId;
+    $http.get('./phones.json').then(successCallback, errorCallback);
+    function successCallback(response){      
+      _.map(response.data, function(arr){        
+        if(arr.id === $routeParams.phoneId){
+          $scope.phone = arr;
+        }
+      })
+    };
+    function errorCallback(error){
+      console.log(error);
+    };
+}]);
 
 app.config(["$routeProvider","$locationProvider", function($routeProvider,$locationProvider){
   $locationProvider.html5Mode({
@@ -14,15 +45,19 @@ app.config(["$routeProvider","$locationProvider", function($routeProvider,$locat
       templateUrl:'main.html',
       controller: 'appCtrl'
     })
-    .when('/about',{
+    .when('/phones.html',{
+      templateUrl:'phones.html',
+      controller: 'phonesCtrl'
+    })
+    .when('/about.html',{
       templateUrl:'about.html',
       controller: 'aboutCtrl'
     })
-    .when('/services',{
+    .when('/services.html',{
       templateUrl:'services.html',
       controller: 'servicesCtrl'
     })
-    .when('/contact',{
+    .when('/contact.html',{
       templateUrl:'contact.html',
       controller: 'contactCtrl'
     })
@@ -33,47 +68,4 @@ app.config(["$routeProvider","$locationProvider", function($routeProvider,$locat
     .otherwise({
       redirectTo: '/'
     })
-}]);
-
-app.controller("appCtrl", ["$scope","$http","$location",function($scope,$http,$location){
-  
-
-  $http.get('./phones.json').then(successCallback, errorCallback);
-
-  function successCallback(response){
-    $scope.phones = response.data;
-  };
-
-  function errorCallback(error){
-    console.log(error);
-  };
-  
-}]);
-
-app.controller("aboutCtrl", ["$scope","$http","$location",function($scope,$http,$location){
-}]);
-app.controller("servicesCtrl", ["$scope","$http","$location",function($scope,$http,$location){
-}]);
-app.controller("contactCtrl", ["$scope","$http","$location",function($scope,$http,$location){
-}]);
-app.controller("phoneCtrl", ["$scope","$http","$location", "$routeParams",function($scope,$http,$location,$routeParams){
-    $scope.phoneId = $routeParams.phoneId;
-
-    $http.get('./phones.json').then(successCallback, errorCallback);
-
-    function successCallback(response){
-      
-      _.map(response.data, function(arr){
-        
-        if(arr.id === $routeParams.phoneId){
-          $scope.phone = arr;
-        }
-
-      })
-
-    };
-
-    function errorCallback(error){
-      console.log(error);
-    };
 }]);
