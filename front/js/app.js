6f83650d27ba3fb79abcfa91745fba54cb1fcd27
GagarinAP <gagarin.web@gmail.com>
2017-03-05 13:23:58 +0200

    147.1

--- front/js/app.js
+++ front/js/app.js
@@ -3,60 +3,44 @@ var app = angular.module("app", [
   "ui.bootstrap",
   "ngResource"
 ]);
-app.controller("sportsStoreCtrl", function ($scope) {
- 
-  $scope.data = {
-    products: [
-      { name: "Product #1", description: "A product", category: "Category #1", price: 100 },
-      { name: "Product #2", description: "A product", category: "Category #1", price: 110 },
-      { name: "Product #3", description: "A product", category: "Category #2", price: 210 },
-      { name: "Product #4", description: "A product", category: "Category #3", price: 202 },
-      { name: "Product #5", description: "A product", category: "Category #1", price: 170 },
-      { name: "Product #6", description: "A product", category: "Category #1", price: 170 },
-      { name: "Product #7", description: "A product", category: "Category #2", price: 270 },
-      { name: "Product #8", description: "A product", category: "Category #3", price: 272 },
-      { name: "Product #9", description: "A product", category: "Category #1", price: 170 },
-      { name: "Product #10", description: "A product", category: "Category #1", price: 170 },
-      { name: "Product #11", description: "A product", category: "Category #2", price: 270 },
-      { name: "Product #12", description: "A product", category: "Category #3", price: 272 }
-    ]
+//=========const=============
+app.constant("productListActiveClass", "btn-primary");
+app.constant("productListPageCount", 3);
+app.constant("dataUrl", "./json/todo.json");
+//=========ctrl============
+app.controller("sportsStoreCtrl", function ($scope,$http,dataUrl) { 
+  $scope.data = {};  
+  $http.get(dataUrl).then(good,bad);
+  function good(response) {
+    console.log(response.data);
+    $scope.data.products = response.data;
   };
-
+  function bad(error) {
+    $scope.data.error = error;
+  };
 });
-
-app.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
- 
+app.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) { 
   var selectedCategory = null;
-
   $scope.selectedPage = 1;
-  $scope.pageSize = productListPageCount;
-   
+  $scope.pageSize = productListPageCount;   
   $scope.selectCategory = function (newCategory) {
     selectedCategory = newCategory;
     $scope.selectedPage = 1;
   }
-
   $scope.selectPage = function (newPage) {
     $scope.selectedPage = newPage;
-  }
-   
+  }   
   $scope.categoryFilterFn = function (product) {
     return selectedCategory == null || product.category == selectedCategory;
   }
-
   $scope.getCategoryClass = function (category) {
     return selectedCategory == category ? productListActiveClass : "";
   }
-
   $scope.getPageClass = function (page) {
     return $scope.selectedPage == page ? productListActiveClass : "";
   }
-
 });
-
-app.constant("productListActiveClass", "btn-primary");
-app.constant("productListPageCount", 3);
-
+//============filters=============
 app.filter("unique", function () {
   return function (data, propertyName) {
     if (angular.isArray(data) && angular.isString(propertyName)) {
@@ -71,9 +55,9 @@ app.filter("unique", function () {
         }
       }
         return results;
-      } else {
-        return data;
-      }
+    } else {
+      return data;
+    }
   }
 });
 app.filter("range", function ($filter) {
