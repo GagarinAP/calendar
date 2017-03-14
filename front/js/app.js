4a1a60a9602baee5e6a088fade351b6757db4c10
GagarinAP <gagarin.web@gmail.com>
2017-03-14 21:27:16 +0200

    add form

--- front/js/app.js
+++ front/js/app.js
@@ -5,10 +5,33 @@ var app = angular.module("app", [
 ]);
 
 app.controller("appCtrl", function($scope){
-  $scope.name = 'Calendar Period';
-});
+  var model = {    
+    items: [{ name: "Julia", done: false, period: 4, begindate: "2017-03-09T22:00:00.000Z", periodic: 27 }]
+  };
+  $scope.todo = model;
 
-app.controller('DatepickerDemoCtrl', function ($scope) {
+  $scope.incompleteCount = function () {
+    var count = 0;
+    angular.forEach($scope.todo.items, function (item) {
+      if (!item.done) { 
+        count++; 
+      }
+    });
+    return count;
+  }
+   
+  $scope.warningLevel = function () {
+  return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+  }
+   
+  $scope.addNewItem = function (Name,Period,DateBegin,Periodic) {
+    $scope.todo.items.push({ name: Name, done: false, period: Period, begindate: DateBegin, periodic: Periodic});
+  }
+
+
+
+  
+
   $scope.today = function() {
     $scope.dt = new Date();
   };
@@ -25,25 +48,28 @@ app.controller('DatepickerDemoCtrl', function ($scope) {
   };
   
   $scope.period = {
-    value: 5,
+    value: 4,
     date: new Date()
   }
 
   var Arr = [];
   for(var i = 0; i < $scope.period.value; ++i){
     $scope.period.date.setDate($scope.period.date.getDate() + 1);
-    Arr.push({date: $scope.period.date + 1, status: 'partially'});
+    Arr.push({date: $scope.period.date + 1, status: 'partially',class: 'bbbbbbbbbbbbbbb'});
   }
 
   $scope.events = Arr;
 
   function getDayClass(data) {
-    var date = data.date,
-      mode = data.mode;
+
+    var date = data.date, mode = data.mode;
+
     if (mode === 'day') {
+
       var dayToCheck = new Date(date).setHours(0,0,0,0);
 
       for (var i = 0; i < $scope.events.length; i++) {
+
         var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
 
         if (dayToCheck === currentDay) {
@@ -54,4 +80,5 @@ app.controller('DatepickerDemoCtrl', function ($scope) {
 
     return '';
   }
+
 });
